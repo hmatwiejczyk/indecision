cd680d07cbde47d5f9daec7aa5855cc914e0d9a9
Hubert Matwiejczyk <hmatwiejczyk@sii.pl>
2019-10-21 16:01:33 +0200

    counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,56 @@
 'use strict';
 
-console.log('app is running');
+var app = {
+  title: 'Indecision App',
+  subtitle: 'some info',
+  options: ['One', 'Two']
+};
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'this is jsx!'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'here your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  ),
+  React.createElement(
+    'form',
+    null,
+    React.createElement('input', { text: 'text', name: 'option' }),
+    React.createElement(
+      'button',
+      null,
+      'Add Option'
+    )
+  )
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
-console.log('app is running');
+const app = {
+  title: 'Indecision App',
+  subtitle: 'some info',
+  options: ['One', 'Two']
+};
 
-const template = <p>this is jsx!</p>;
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>Age: {app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'here your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+    <form>
+      <input text="text" name="option" />
+      <button>Add Option</button>
+    </form>
+  </div>
+);
+
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
