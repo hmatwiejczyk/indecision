6b676c6bad9d92c884f1ba2332666848be5daeb7
Hubert Matwiejczyk <hmatwiejczyk@sii.pl>
2019-10-21 14:29:42 +0200

    add project dependenencies, basic setup

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('app is running');
+
+var template = React.createElement(
+  'p',
+  null,
+  'this is jsx!'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('app is running');
+
+const template = <p>this is jsx!</p>;
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
